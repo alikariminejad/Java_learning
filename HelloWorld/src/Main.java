de520189f7b1b8a13a0ed441680d847be57ef674
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-12 23:20:43 +0330

    variable types

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        // byte --> 1 byte -128 -- 127
+        // short --> 2 byte -32k -- 32k
+        // int --> 4 byte -2b -- 2b
+        // long --> 8 byte
+
+        // float --> 4 byte
+        // double --> 8 byte
+
+        // char
+        // boolean 1 byte (true or false)
+
+        byte age = 26;
+        int viewCount = 123_456_789;  // you can use underscore to read big numbers easily
+        long viewCount2 = 2213_325_653L; // you should add L or l to number to identify this as long instead of int
+        float price = 18.99F;
+        boolean darkMode = false;
+
     }
 }
