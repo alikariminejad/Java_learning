6433bcee37eb1903341b5bae68dafdcce470eaa5
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-18 20:31:59 +0330

    reference type

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        byte age = 26;
+//        byte age = 26;
         Date now = new Date();
         System.out.println(now);
 
