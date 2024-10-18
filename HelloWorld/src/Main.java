6fe04f01feafb0a7c1d4ac00494b8dbeec0ca52d
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-18 20:48:12 +0330

    primitive vs reference type

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,10 +1,15 @@
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-//        byte age = 26;
-        Date now = new Date();
-        System.out.println(now);
-
+    //        byte x = 1;
+    //        byte y = x;
+    //        x = 2;
+    //        System.out.println(y); // = 1
+        Point point1 = new Point(5, 5);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);  // = java.awt.Point[x=2,y=5]
     }
 }
