0905138995d26c8e7ff39f31ba59c4470ec28ec6
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-19 17:31:45 +0330

    Creating a simple array

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,18 +1,13 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        String message = "Hello World from java" + "!!!";
-        System.out.println(message.length());  // message.indexOf("H") = 0
-        System.out.println(message.replace("!", "$"));
-        String message2 = "   new string   ";
-        System.out.println(message2.trim());
-        String message3 = "Hello double \"quotation\"";
-        System.out.println(message3);
-        String message4 = "c:\\windows\\drivers\\new...";
-        System.out.println(message4);
-        String message5 = "when i use backslash and \"n\" \n it goes to the next line!!!";
-        System.out.println(message5);
+//        int[] numbers = new int[5]; // this array has 5 elements
+        int[] numbers = {1, 6, 3, 40, 5};
+        Arrays.sort(numbers);
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
     }
 }
