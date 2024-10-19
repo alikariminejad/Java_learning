f1a3a2432e0a1220cf2232f303f176d7a975db2c
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-19 15:45:46 +0330

    working with Strings

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,13 +3,11 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-    //        byte x = 1;
-    //        byte y = x;
-    //        x = 2;
-    //        System.out.println(y); // = 1
-        Point point1 = new Point(5, 5);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);  // = java.awt.Point[x=2,y=5]
+        String message = "Hello World from java" + "!!!";
+        System.out.println(message.length());  // message.indexOf("H") = 0
+        System.out.println(message.replace("!", "$"));
+        String message2 = "   new string   ";
+        System.out.println(message2.trim());
+
     }
 }
