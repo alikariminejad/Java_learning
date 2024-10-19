c8c6f8d20533ee41bbd4ec3329bee02490c018ce
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-19 15:54:51 +0330

    scape sequence

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -8,6 +8,11 @@ public class Main {
         System.out.println(message.replace("!", "$"));
         String message2 = "   new string   ";
         System.out.println(message2.trim());
-
+        String message3 = "Hello double \"quotation\"";
+        System.out.println(message3);
+        String message4 = "c:\\windows\\drivers\\new...";
+        System.out.println(message4);
+        String message5 = "when i use backslash and \"n\" \n it goes to the next line!!!";
+        System.out.println(message5);
     }
 }
