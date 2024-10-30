6f967490c19a73903feafe74c15477bda082c287
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-31 00:03:09 +0330

    Number format

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,18 +1,20 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-//        int result = Math.round(1.6F);
-//        double result = Math.ceil(1.1F);
-//        double result = Math.floor(11.9F);
-//        double result = Math.max(1, 5);
-//        double result = Math.min(2, 5);
-//        double result = Math.random();   // random number between 0 and 1
-//        double result = Math.random() * 2; // random number between 0 and 2
-        int result = (int) (Math.random() * 24);
-//        int result = (int) Math.round(Math.random() * 100);
-        System.out.println(result);
+//        NumberFormat currency = NumberFormat.getCurrencyInstance();
+//        String result = currency.format(12242342342.234234);
+//        System.out.println(result);  // $12,242,342,342.23
+
+
+//        NumberFormat percent = NumberFormat.getPercentInstance();
+//        String result = percent.format(0.35);
+//        System.out.println(result); // 35%
+        // or you can write this
+        String result = NumberFormat.getPercentInstance().format(0.35);
+        System.out.println(result); // 35%
     }
 }
